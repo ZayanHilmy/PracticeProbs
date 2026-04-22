4a6cb5fa7b36da8b8eb302a1a4eaffb79eac8e92
Zayan <161733338+ZayanHilmy@users.noreply.github.com>
2026-04-22 22:14:10 +0800

    Add files via upload

---
+++ Pandas/babynames.py
@@ -0,0 +1,17 @@
+#You and your spouse decided to let the internet name your next child. 
+#You’ve asked the great people of the web to submit their favorite names, 
+#and you’ve compiled their submissions into a Series called babynames.
+
+import numpy as np
+import pandas as pd
+
+babynames = pd.Series([
+    'Jathonathon', 'Zeltron', 'Ruger', 'Phreddy', 'Ruger', 'Chad', 'Chad',
+    'Ruger', 'Ryan', 'Ruger', 'Chad', 'Ryan', 'Phreddy', 'Phreddy', 'Phreddy',
+    'Mister', 'Zeltron', 'Ryan', 'Ruger', 'Ruger', 'Jathonathon',
+    'Jathonathon', 'Ruger', 'Chad', 'Zeltron'], dtype='string')
+
+
+filtered_babynames = babynames[babynames.isin(['Chad','Ruger','Zeltron'])]
+
+print(filtered_babynames.value_counts())
